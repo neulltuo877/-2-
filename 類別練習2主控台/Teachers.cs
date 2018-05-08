f0747766bd3256302293d529b64a81f6f2ccb2d4
ChangLi Li <milplle1516@outlook.com>
2018-05-08 15:40:43 +0800

    -2-

---
+++ 類別練習2主控台/Teachers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 類別練習2主控台
+{
+    class Teachers
+    {
+        private string name;
+        private string dagree;
+        private string expertise;
+        private int phone;
+        private string email;
+
+        public string Name
+        {
+            get => name == null ? "null" : name;
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    name = "null";
+                }
+                else
+                {
+                    name = value;
+                }
+
+            }
+        }
+
+
+    }
+}
